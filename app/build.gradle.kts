8d578dacf8ca629660957f3234ff69e04df550c4
kashroguel <kjtgloriani@gmail.com>
2025-04-05 01:27:06 -0400

    Added the gson library and sync it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.gson) //Added the gson library
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
